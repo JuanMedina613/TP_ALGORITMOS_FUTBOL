b1bfc0b7141bdb9be4d64c632e1f4c4166c78727
AriadnaJV <ariadnavianna@gmail.com>
2026-06-20 18:40:24 -0300

    ind_crear e ind_vaciar

--- TP_Algoritmos&EstructurasdeDatosFC/Bibliotecas/Indice.c
+++ TP_Algoritmos&EstructurasdeDatosFC/Bibliotecas/Indice.c
@@ -2,6 +2,9 @@
 //****************************************************************************************************//
 void ind_crear(t_indice* ind, size_t tam_clave, int (*cmp)(const void*, const void*))
 {
+    CrearArbol(&ind->arbol);
+    ind->tamClave = tam_clave;
+    ind->cmp = cmp;
 }
 //****************************************************************************************************//
 int ind_insertar (t_indice* ind, void *clave, unsigned nro_reg)
@@ -57,7 +60,7 @@ int ind_grabar(const t_indice* ind, const char* path)
 //****************************************************************************************************//
 void ind_vaciar(t_indice* ind)
 {
-
+    EliminarArbol(&ind->arbol);
 }
 //****************************************************************************************************//
 int ind_recorrer (const t_indice* ind, void (*accion)(void *, unsigned, unsigned, void *), void* param)
